c8686f1e10c741a36295719afbfe4b6b240a79f1
Piyush Mehta <77567412+piyux@users.noreply.github.com>
2021-09-08 20:29:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
-  
+from flask import Flask, render_template, request
+import joblib
+
 app = Flask(__name__)
-  
+
+model = joblib.load('./model.x')
+
 @app.route("/")
-def home():
-        return "<h1>hello wworld</h1>"
+def root():
+    return render_template('index.html')
+    
+@app.route('/predict',methods=['GET'])
+def predict():
+    name = request.args.get('name')
+    exp = request.args.get('exp')
+    sal = str(model.predict([[exp]]))
+    sal = round(float(sal[2:-2]))
+    return render_template('predict.html',data=[name,sal])
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('error.html'),404
+ 
+#Handling error 500 and displaying relevant web page
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('error.html'),500
+
+if __name__ == '__main__':
+  app.run(debug=True)
